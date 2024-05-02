d29bff79d6994cfec0fbd4fb911ca840e68c23da
Jayne Valverde <jaynevalverde@gmail.com>
2024-05-02 13:41:16 -0500

    css, java onclick, home page

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,20 @@
+// -- navbar --
 let navbar = document.querySelector('.navbar');
 
 document.querySelector('#menu-btn').onclick = () =>{
     navbar.classList.toggle('active');
+    searchForm.classList.remove('active');
+}
+
+// -- search bar --
+let searchForm = document.querySelector('.search-form');
+
+document.querySelector('#search-btn').onclick = () =>{
+    searchForm.classList.toggle('active');
+    navbar.classList.remove('active');
+}
+
+window.onscroll = () =>{
+  navbar.classList.remove('active');
+  searchForm.classList.remove('active');
 }
