118c2f4c4eee13e29fedb867f8057e66bdd5fa7e
Jayne Valverde <jaynevalverde@gmail.com>
2024-05-08 19:24:23 -0500

    contact form function

--- assets/script.js
+++ assets/script.js
@@ -3,20 +3,11 @@ let navbar = document.querySelector('.navbar');
 
 document.querySelector('#menu-btn').onclick = () =>{
     navbar.classList.toggle('active');
-    searchForm.classList.remove('active');
 }
 
-// -- search bar --
-let searchForm = document.querySelector('.search-form');
-
-document.querySelector('#search-btn').onclick = () =>{
-    searchForm.classList.toggle('active');
-    navbar.classList.remove('active');
-}
 
 window.onscroll = () =>{
   navbar.classList.remove('active');
-  searchForm.classList.remove('active');
 }
 
 document.getElementById("workBtn").onclick = function () {
